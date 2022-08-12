1a002ee10be1248a54880a7f0be4ee0e7bfdf233
Arseny Chistyakov <samson200289@mail.ru>
2022-08-13 02:16:31 +0300

    added Users.js(component) *frontend separately backend separately

--- service/settings.py
+++ service/settings.py
@@ -13,6 +13,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -27,21 +32,16 @@ INSTALLED_APPS = [
     'corsheaders',
     'users',
 ]
-# corsheaders
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'service.urls'
