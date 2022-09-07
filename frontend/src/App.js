8358a7ce5c60a6061f243dd94fd90f6374ef16fc
Arseny Chistyakov <samson200289@mail.ru>
2022-09-08 02:00:17 +0300

    added Auth(JWT) component localized files install universal-cookie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import {ProjectDetail, ProjectList} from './components/Project.js'
 import TODOList from "./components/TODO";
 import Footer from "./components/Footer";
 import Navbar from "./components/Menu";
+import LoginForm from "./components/Auth";
 
 
 const DOMAIN = 'http://127.0.0.1:8000/api/'
@@ -18,7 +19,7 @@ const get_url = (url) => `${DOMAIN}${url}`
 function NotFound404() {
     let pageLocation = useLocation()
     return (
-        <div>
+        <div className='container'>
             <h1 className='mt-4 text-lg-center'>Страница по адресу '{pageLocation.pathname}' не найдена</h1>
         </div>
     )
@@ -35,25 +36,75 @@ class App extends React.Component {
             ],
             users: [],
             projects: [],
-            todos: []
+            TODOs: [],
+            auth: {username: '', is_login: false}
         }
     }
 
+    login(username, password) {
+        axios.post(get_url('token/'), {username: username, password: password})
+            .then(response => {
+                const result = response.data
+                const access = result.access
+                const refresh = result.refresh
+                localStorage.setItem('login', username)
+                localStorage.setItem('access', access)
+                localStorage.setItem('refresh', refresh)
+                this.setState({'auth': {username: username, is_login: true}})
+                this.load_data()
+            }).catch(error => {
+            if (error.response.status === 401) {
+                alert('Неверный логин или пароль')
+            } else {
+                console.log(error)
+            }
+        })
+    }
+
+
+    logout() {
+        localStorage.setItem('login', '')
+        localStorage.setItem('access', '')
+        localStorage.setItem('refresh', '')
+        this.setState({'auth': {username: '', is_login: false}})
+    }
+
+
+    load_data() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.state.auth.is_login) {
+            const token = localStorage.getItem('access')
+            headers['Authorization'] = 'Bearer ' + token
+        }
+        axios.get(get_url('users/'), {headers})
+            .then(response => {
+                this.setState({users: response.data.results})
+                console.log(response.data)
+            }).catch(error =>
+            console.log(error)
+        )
+
+        axios.get(get_url('projects/'), {headers})
+            .then(response => {
+                this.setState({projects: response.data.results})
+            }).catch(error => console.log(error))
+
+        axios.get(get_url('TODOs/'), {headers})
+            .then(response => {
+                console.log(response.data)
+                this.setState({TODOs: response.data.results})
+            }).catch(error =>
+            console.log(error)
+        )
+    }
+
     componentDidMount() {
-        axios.get(get_url('users/'))
-            .then(response => {
-                this.setState({'users': response.data.results})
-            }).catch(error => console.log(error))
-
-        axios.get(get_url('projects/'))
-            .then(response => {
-                this.setState({'projects': response.data.results})
-            }).catch(error => console.log(error))
-
-        axios.get(get_url('TODOs/'))
-            .then(response => {
-                this.setState({'TODOs': response.data.results})
-            }).catch(error => console.log(error))
+        const username = localStorage.getItem('login')
+        if ((username !== "") && (username != null)) {
+            this.setState({'auth': {username: username, is_login: true}}, () => this.load_data())
+        }
     }
 
     render() {
@@ -61,10 +112,13 @@ class App extends React.Component {
             <div className="App">
                 <BrowserRouter>
                     <header>
-                        <Navbar navbarItems={this.state.navbarItems}/>
+                        <Navbar navbarItems={this.state.navbarItems} auth={this.state.auth}
+                                logout={() => this.logout()}/>
                     </header>
                     <main role="main" className="flex-shrink-0">
                         <Routes>
+                            <Route exact path='/login' element={<LoginForm
+                                login={(username, password) => this.login(username, password)}/>}/>
                             <Route exact path='/' element={<UserList users={this.state.users}/>}/>
                             <Route exact path='/users' element={<UserList users={this.state.users}/>}/>
                             <Route exact path='/TODOs' element={<TODOList TODOs={this.state.TODOs}/>}/>
