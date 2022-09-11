7d181f6c79fd8c9d200e337fc952710800152942
Arseny Chistyakov <samson200289@mail.ru>
2022-09-11 23:17:10 +0300

    added redoc(docs) install drf_yasg

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from TODO.views import TODOModelViewSet, ProjectModelViewSet
 from users.views import CustomUserViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TODO",
+        default_version='0.1',
+        description="Documentation to out project",
+        contact=openapi.Contact(email="samson200289@mail.ru"),
+        license=openapi.License(name="MIT License"), ),
+    public=True,
+    permission_classes=[permissions.IsAdminUser], )
 
 router = DefaultRouter()
 router.register('users', CustomUserViewSet, basename='users')
@@ -22,4 +35,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # Docs
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
